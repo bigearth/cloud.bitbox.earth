353dc97d72ba6c12137047e61c3c1962230ea494
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-22 15:34:18 -0700

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
 
 import './App.css';
 let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
@@ -81,33 +80,12 @@ class App extends Component {
   }
 
   render() {
-    let addresses = [];
-    for(let i = 0; i < 10; i++) {
-      let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
-      addresses.push(<li key={i}>m/44&rsquo;/145&rsquo;/0&rsquo;/0/{i}: {BITBOX.HDNode.toCashAddress(childNode)}</li>);
-    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello BITBOX</h1>
+          <img src="https://www.bitbox.earth/assets/logo.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">BITBOX Cloud</h1>
         </header>
-        <div className='App-content'>
-          <h2>BIP44 $BCH Wallet</h2>
-          <h3>256 bit {lang} BIP39 Mnemonic:</h3> <p>{this.state.mnemonic}</p>
-          <h3>BIP44 Account</h3>
-          <p>
-            <code>
-            "m/44'/145'/0'"
-            </code>
-          </p>
-          <h3>BIP44 external change addresses</h3>
-          <ul>
-            {addresses}
-          </ul>
-          <h3>Transaction raw hex</h3>
-          <p>{this.state.hex}</p>
-        </div>
       </div>
     );
   }
