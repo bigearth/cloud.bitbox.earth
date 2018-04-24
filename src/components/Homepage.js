affe4f9e70c724aedb5d78ec8d31a881f1c5018c
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-24 05:45:08 -0700

    Cleanup

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -49,7 +49,7 @@ class Homepage extends Component {
                 <li>No customer support</li>
               </ul>
 
-              <button className="button-choose pure-button">Choose</button>
+              <button className="button-choose pure-button">Coming Soon</button>
             </div>
           </div>
 
@@ -69,7 +69,7 @@ class Homepage extends Component {
                 <li>Standard customer support</li>
               </ul>
 
-              <button className="button-choose pure-button">Choose</button>
+              <button className="button-choose pure-button">Coming Soon</button>
             </div>
           </div>
 
@@ -89,47 +89,7 @@ class Homepage extends Component {
                 <li>Premium customer support</li>
               </ul>
 
-              <button className="button-choose pure-button">Choose</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="information pure-g">
-          <div className="pure-u-1 pure-u-md-1-2">
-            <div className="l-box">
-              <h3 className="information-head">Full Node</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation.
-              </p>
-            </div>
-          </div>
-
-          <div className="pure-u-1 pure-u-md-1-2">
-            <div className="l-box">
-              <h3 className="information-head">Pay monthly or annually</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.
-              </p>
-            </div>
-          </div>
-
-          <div className="pure-u-1 pure-u-md-1-2">
-            <div className="l-box">
-              <h3 className="information-head">24/7 customer support</h3>
-              <p>
-                Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-
-          <div className="pure-u-1 pure-u-md-1-2">
-            <div className="l-box">
-              <h3 className="information-head">Cancel your plan anytime</h3>
-              <p>
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <button className="button-choose pure-button">Coming Soon</button>
             </div>
           </div>
         </div>
